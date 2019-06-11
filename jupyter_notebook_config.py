4adab97a82efde3f281072d8d962078eac5ca906
Lorenzo Raimondi <lorenzo.raimondi23@gmail.com@>
2019-06-11 20:29:47 +0000

    Ops, I fixed ch04

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -3,7 +3,7 @@ c = get_config()
 # Notebook config this is where you saved your pem cert
 # c.NotebookApp.certfile = u'/home/ubuntu/certs/mycert.pem'
 # Run on all IP addresses of your instance
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = 'localhost'
 # Don't open browser by default
 c.NotebookApp.open_browser = False  
 # Fix port to 8888
